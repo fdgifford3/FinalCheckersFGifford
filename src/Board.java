9cf86936eebad47141b12bcf0f1bf6730c4b4914
fdgifford3 <fdgifford3@gmail.com>
2019-10-19 13:45:16 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -31,6 +31,7 @@ public class Board extends JPanel implements ActionListener, MouseListener {
 		legalMoves = moveLegality.getLegalMoves(board.getState());
 	}
 	
+	//Did not use these
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
@@ -49,42 +50,38 @@ public class Board extends JPanel implements ActionListener, MouseListener {
 
 	}
 
+	// Get coordinates for where the user clicks and do the reverse of the math that
+	// Was used to paint the squares on the board to decide which square was clicked
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		int col = (arg0.getX() - 12) / 60;
-		System.out.println(arg0.getX());
         int row = (arg0.getY() - 12) / 60;
-        System.out.println(arg0.getY());
         if (col >= 0 && col < 8 && row >= 0 && row < 8)
             clickSquare(row,col);
-
-
 	}
 
+	//Did not use
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
 
+	//Just a new game button, repaint is causing a visual bug that I am having trouble figuring out
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
         Object src = arg0.getSource();
         if (src == newGameButton) {
         	board.placePieces();
+            legalMoves = moveLegality.getLegalMoves(board.getState());
         	turnText.setText(board.turnText);
         	repaint();
         }
 	}
 	
 	public void clickSquare(int row, int col) {
-		System.out.println(row);
-		System.out.println(col);
-		for (int i = 0; i < legalMoves.length; i++) {
-            }
-		
-		
-		
+
+		// Check to see if the square clicked is in the legal Moves array, if so, set the selected row and column
         for (int i = 0; i < legalMoves.length; i++) {
             if (legalMoves[i].fromRow == row && legalMoves[i].fromCol == col) {
                 selectedRow = row;
@@ -94,7 +91,8 @@ public class Board extends JPanel implements ActionListener, MouseListener {
             }
         }
 
-
+        //Now, check to see if the second square clicked is in the legal Moves array for the first selected square,
+        //If so, make the move
         for (int i = 0; i < legalMoves.length; i++) {
             if (legalMoves[i].fromRow == selectedRow && legalMoves[i].fromCol == selectedCol
             && legalMoves[i].toRow == row && legalMoves[i].toCol == col) {
@@ -126,14 +124,16 @@ public class Board extends JPanel implements ActionListener, MouseListener {
 
         if (board.getState().equals(board.getRedTurnState())) {
         	board.getState().turnFinished();
+        	turnText.setText(board.getState().getTurnText());
             legalMoves = moveLegality.getLegalMoves(board.getState());
             if (legalMoves == null) {
             	board.gameOver = true;
-            	board.getState().turnFinished();
+            	board.getState().turnFinished();            	
             	turnText.setText("Black has no moves. Game Over  Red wins.");
             }
         } else if(board.getState().equals(board.getBlackTurnState())) {
         	board.getState().turnFinished();
+        	turnText.setText(board.getState().getTurnText());
             legalMoves = moveLegality.getLegalMoves(board.getState());
             if (legalMoves == null) {
             	board.gameOver = true;
@@ -152,8 +152,9 @@ public class Board extends JPanel implements ActionListener, MouseListener {
 	public void paintComponent(Graphics g) {
 		
         g.setColor(Color.black);
-        g.drawRect(0,0,getSize().width-1,getSize().height-1);
+        g.drawRect(0, 0, getWidth()-1, getHeight()-1);
         g.drawRect(1,1,getSize().width-2,getSize().height-2);
+
         
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
